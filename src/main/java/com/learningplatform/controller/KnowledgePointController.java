2111a53776aa86af9bbf0f38e472aaab7c520fb2
LCR <lichaoran20050211@icloud.com>
2026-04-30 16:25:07 +0800

    feat:知识点详情页面实现(前后端)

--- src/main/java/com/learningplatform/controller/KnowledgePointController.java
+++ src/main/java/com/learningplatform/controller/KnowledgePointController.java
@@ -1,7 +1,11 @@
 package com.learningplatform.controller;
 
 import com.learningplatform.common.Result;
+import com.learningplatform.entity.CodeExample;
+import com.learningplatform.entity.KnowledgeContent;
 import com.learningplatform.entity.KnowledgePoint;
+import com.learningplatform.service.CodeExampleService;
+import com.learningplatform.service.KnowledgeContentService;
 import com.learningplatform.service.KnowledgePointService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +18,12 @@ public class KnowledgePointController {
 
     @Autowired
     private KnowledgePointService knowledgePointService;
+
+    @Autowired
+    private KnowledgeContentService knowledgeContentService;
+
+    @Autowired
+    private CodeExampleService codeExampleService;
 
     @GetMapping("/list")
     public Result<List<KnowledgePoint>> list() {
@@ -31,5 +41,17 @@ public class KnowledgePointController {
     public Result<Void> save(@RequestBody KnowledgePoint point) {
         knowledgePointService.save(point);
         return Result.success();
+    }
+
+    @GetMapping("/{knowledgeId}/contents")
+    public Result<List<KnowledgeContent>> getContents(@PathVariable Long knowledgeId) {
+        List<KnowledgeContent> contents = knowledgeContentService.listByKnowledgePointId(knowledgeId);
+        return Result.success(contents);
+    }
+
+    @GetMapping("/{knowledgeId}/codes")
+    public Result<List<CodeExample>> getCodes(@PathVariable Long knowledgeId) {
+        List<CodeExample> codes = codeExampleService.listByKnowledgePointId(knowledgeId);
+        return Result.success(codes);
     }
 }
